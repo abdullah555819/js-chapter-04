46c43ed42a9c9ea42a75a7a0f8ffce94b2629ea1
abdullah555819 <abdullahmemonam1992104@gmail.com>
2024-02-17 02:22:30 +0500

    done

---
+++ app.js
@@ -0,0 +1,18 @@
+// variable declare
+var var1, var2, var3;
+
+// legal veriables
+var myVar;
+var $var;
+var _var;
+var camelCase;
+var _my_name;
+
+
+// illegal variable
+var 1var
+var Rose 
+var @myname
+var my name
+var month&year
+var birth-year
